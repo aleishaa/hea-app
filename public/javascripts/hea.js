eba7ddd17d8cf19059df80c91ebb4d499befb433
Aleisha Amohia <aleisha@catalyst.net.nz>
2014-12-19 07:38:52 +1300

    Moving information into corresponding chart boxes

--- public/javascripts/hea.js
+++ public/javascripts/hea.js
@@ -16,18 +16,20 @@ function build_barchart (params) {
   var width = params.width || 400;
   var bar_padding = 1;
   var bar_height = 20;
+  var caption1 = params.caption1;
+  var caption2 = params.caption2;
 
   var max_variable_name_length = 20;
   var values = [], variables = [];
   $(data).each(function(){
-    values.push(Number(this.value));
-    variables.push(this.name);
+  values.push(Number(this.value));
+  variables.push(this.name);
   });
 
   var height = bar_height * variables.length;
 
   var left_width = 135;
-
+  
   var yRangeBand = bar_height + 2;
   y = function(i) { return yRangeBand * i; };
 
@@ -40,23 +42,47 @@ function build_barchart (params) {
     .append('svg')
     .attr('class', 'chart')
     .attr('width', left_width + width + 40)
-    .attr('height', (bar_height) * variables.length + 30)
+    .attr('height', (bar_height) * variables.length + 71)
     .append("g")
     .attr("transform", "translate(-10, 20)");
- 
+
   chart.selectAll("rect")
     .data(values)
-    .enter().append("rect")
+    .enter()
+    .append("rect")
     .attr("x", left_width)
     .attr("y", function(d, i) { return y(i) })
     .attr("width", x)
-    .attr("height", bar_height);
- 
+    .attr("height", bar_height)
+    .attr("transform", "translate(-10, 40)"); 
+
+  chart
+    .append("text")
+    .attr("x", width /2 )
+    .attr("y", 22)
+    .attr("dy", -6)
+    .attr("text-anchor", "start")
+    .attr("font-size", 14)
+    .attr("transform", "translate(-75, 0)")
+    .style("fill", "#333")
+    .text(caption1);
+
+  chart
+    .append("text")
+    .attr("x", width /2 )
+    .attr("y", 37)
+    .attr("dy", -6)
+    .attr("text-anchor", "start")
+    .attr("font-size", 14)
+    .attr("transform", "translate(-75, 0)")
+    .style("fill", "#333")
+    .text(caption2);
+
   chart
     .append("text")
     .attr("class", "title")
     .attr("x", width /2 )
-    .attr("y", 0)
+    .attr("y", 2)
     .attr("dy", -6)
     .attr("text-anchor", "middle")
     .attr("font-size", 14)
@@ -64,24 +90,29 @@ function build_barchart (params) {
 
   chart.selectAll("text.value")
     .data(values)
-    .enter().append("text")
-    .attr("x", function(d) { var xx = x(d); if ( xx < 11 ) xx = 11; return xx + left_width - 1;  })
+    .enter()
+    .append("text")
+    .attr("x", function(d) { var xx = x(d); if ( xx < 11 ) xx = 11; return xx + left_width - 1; })
     .attr("y", function(d, i) { return y(i) + yRangeBand/2;})
     .attr("dx", -5)
     .attr("dy", ".36em")
     .attr("text-anchor", "end")
+    .attr("transform", "translate(-10, 40)")
     .attr('class', 'value')
     .text(String);
- 
+
   chart.selectAll("text.variable")
     .data(variables)
-    .enter().append("text")
+    .enter()
+    .append("text")
     .attr("x", left_width / 2)
     .attr("y", function(d, i){ return y(i) + yRangeBand/2; } )
     .attr("dy", ".36em")
     .attr("text-anchor", "middle")
     .attr('class', 'variable')
-    .text(function(s) { if ( s == null ) s = 'NULL'; if ( s.length > max_variable_name_length ) { return jQuery.trim(s).substring(0,max_variable_name_length).trim(this) + '...'; } else { return s; } });
+    .attr("transform", "translate(0, 40)")
+    .text(function(s) { if ( s == null ) s = 'NULL'; if ( s.length > max_variable_name_length ) { return jQuery.trim(s).substring(0,max_variable_name_length)
+    .trim(this) + '...'; } else { return s; } });
 }
 
 // FIXME The following needs to be fixed!
@@ -97,7 +128,7 @@ function build_donut (params) {
   var color_range = params.color_range || ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
   var outerRadius = params.outerRadius || radius - 10;
   var innerRadius = params.innerRadius || radius - 70;
-
+  
   var color = d3.scale.ordinal()
     .range(color_range);
 
@@ -109,26 +140,29 @@ function build_donut (params) {
     .sort(null)
     .value(function(d) { return d.value; });
 
-  var svg = d3.select('#' + wrapper_id).append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var svg = d3.select('#' + wrapper_id)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var g = svg.selectAll(".arc")
-        .data(pie(data))
-        .enter().append("g")
-        .attr("class", "arc");
+  var g = svg.selectAll(".arc")
+    .data(pie(data))
+    .enter()
+    .append("g")
+    .attr("class", "arc");
 
-    g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.value); });
+  g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) { return color(d.data.value); });
 
-    g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.name + " (" + d.data.value + ")"; });
-
+  g.append("text")
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .style("text-anchor", "middle")
+    .text(function(d) { return d.data.name + " (" + d.data.value + ")"; });
 }
+
+
 
